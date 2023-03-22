8374dfe861eb73a3771e981eb3594aa148083493
luckygit9 <vamshik25@gmail.com>
2023-03-22 10:14:01 -0500

    comm3

---
+++ cypress/e2e/app2/app2.js
@@ -0,0 +1,14 @@
+import {Given, When, Then } from "cypress-cucumber-preprocessor/steps";
+
+Given('when user launched appln',() => {
+    cy.visit("https://www.google.com/")
+
+})
+
+When('user logged in', ()=>{
+    cy.title().should('eq','Google')
+})
+
+Then('user is displayed home screen', ()=>{
+    cy.get("a[aria-label='Gmail (opens a new tab)']").click()
+})
